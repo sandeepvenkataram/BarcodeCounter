a9df2abc6e67ffd816fec3a4fb9d671fa45f0d2a
Sandeep Venkataram <sandeep1988@gmail.com>
2016-03-21 11:30:15 -0700

    forgot to add findUniquesFromBarcodes.pl file in original commit

---
+++ findUniquesFromBarcodes.pl
@@ -0,0 +1,30 @@
+#!/hsgs/projects/petrov/perlroot/perl/bin/perl
+
+use strict;
+use warnings;
+
+my $barcodeFilePrefix = shift;
+
+my %uniqueBarcodePCRDupTracker = ();
+my %uniqueBarcodeCounter = ();
+open(IN,$barcodeFilePrefix.".barcodes");
+while(<IN>){
+	chomp;
+	my ($fwd,$rev,$fwdpcr,$revpcr) = split("\t",$_);
+	my $key = $fwd;
+	my $pcrKey = $fwdpcr."_".$revpcr."\t";
+	if(! defined $uniqueBarcodePCRDupTracker{$key}){
+		$uniqueBarcodeCounter{$key} = 1;
+		$uniqueBarcodePCRDupTracker{$key} = "\t".$pcrKey;
+	}else{
+		next if $uniqueBarcodePCRDupTracker{$key} =~ "\t$pcrKey";
+		$uniqueBarcodeCounter{$key} ++;
+		$uniqueBarcodePCRDupTracker{$key} .= $pcrKey;
+	}
+}
+close(IN);
+open(OUT,">".$barcodeFilePrefix.".uniques");
+foreach my $key(keys %uniqueBarcodeCounter){
+	print OUT $uniqueBarcodeCounter{$key}."\t".$key."\n";
+}
+close(OUT);
